162551eb07a1ee16c04f4e49f703b78d12f57b73
pola0noch <klayriddell@gmail.com>
2024-09-26 17:20:18 +0300

    init

---
+++ module_10_2.py
@@ -0,0 +1,42 @@
+# Домашнее задание по теме "Потоки на классах"
+
+from threading import Thread
+import time
+
+
+class Knight(Thread):
+    def __init__(self, name, power):
+        super().__init__()
+        self.name = name
+        self.power = power
+        self.enemies = 100
+        self.days = 0
+
+    def run(self):
+        print(f"{self.name}, на нас напали!")
+        while self.enemies > 0:
+            self.enemies -= self.power
+            self.days += 1
+            time.sleep(1)
+            print(f"{self.name} сражается {self.days} дней(дня), осталось {self.enemies} воинов.")
+
+        print(f"{self.name} одержал победу спустя {self.days} дней(дня)!")
+
+
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+
+first_knight.start()
+second_knight.start()
+
+first_knight.join()
+second_knight.join()
+
+print("Окончание битв!")
+
+
+
+
+
+
+
